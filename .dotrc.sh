6871883b0ff85eaa1ed00beb2478f34b532d57df
Emanuel de Souza <emanuelcds@gmail.com>
2013-11-27 16:32:53 -0200

    Fix the fixed git ps1 status independent from directory.

--- .dotrc.sh
+++ .dotrc.sh
@@ -2,4 +2,4 @@ source $HOME/git-prompt.sh
 COLOR_BLUE='\[\033[0;36m\]'
 COLOR_GOLD='\[\033[0;33m\]'
 COLOR_DEFAULT='\[\033[0;0m\]'
-PS1="$COLOR_BLUE[\u@\h \W$COLOR_GOLD`__git_ps1`$COLOR_BLUE]\$ $COLOR_DEFAULT"
+PS1="$COLOR_BLUE[\u@\h \W$COLOR_GOLD\$(__git_ps1 ' (%s)')$COLOR_BLUE]\$ $COLOR_DEFAULT"
