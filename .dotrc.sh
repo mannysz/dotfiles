a95a4190e034c33ebfddc41a86af2483edee1adf
Paul Miller <paul@paulmillr.com>
2015-02-18 13:48:09 -0200

    Converted dotfiles for working on os x bash with git completion and added pre-install script for requirements.

--- .dotrc.sh
+++ .dotrc.sh
@@ -1,6 +1,11 @@
-source `dirname $0`/.git-prompt
-source `dirname $0`/.aliases
+source $HOME/.git-prompt
+source $HOME/.aliases
 COLOR_BLUE='\[\033[0;36m\]'
 COLOR_GOLD='\[\033[0;33m\]'
 COLOR_DEFAULT='\[\033[0;0m\]'
 PS1="$COLOR_BLUE[\u@\h \W$COLOR_GOLD\$(__git_ps1 ' (%s)')$COLOR_BLUE]\$ $COLOR_DEFAULT"
+
+# enabling git completion on bash
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
--- .vimrc
+++ .vimrc
@@ -2,4 +2,6 @@ set ai
 set ts=4
 set expandtab
 set colorcolumn=80
+set term=xterm
+syntax on
 highlight ColorColumn ctermbg=8
